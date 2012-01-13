ab26c3839f292425fa3c55d240360134e17029e5
Michael Morris <mtcmorris@gmail.com>
2012-01-14 00:13:02 +1100

    Added validation of content type

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -2,4 +2,6 @@ class Bot < ActiveRecord::Base
   has_attached_file :source
   validates_presence_of :source
   validates_presence_of :player_name
+
+  validates_attachment_content_type :source, content_type: ['application/zip', 'application/x-zip', 'application/x-zip-compressed']
 end
